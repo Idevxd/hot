ca84db29a9d49db3a88f6ea23199ef811ee1c6e1
anam_bactiar <134827499+bactiar291@users.noreply.github.com>
2024-11-04 00:43:29 +0700

    Create rpc.js

---
+++ rpc.js
@@ -0,0 +1,7 @@
+export const mainnetConfig = {
+  networkId: "mainnet",
+  nodeUrl: "https://rpc.mainnet.near.org",
+  walletUrl: "https://wallet.mainnet.near.org",
+  helperUrl: "https://helper.mainnet.near.org",
+  explorerUrl: "https://explorer.mainnet.near.org",
+};
