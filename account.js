a32b83edde0a53d1306064b59c2817e6414dcc02
anam_bactiar <134827499+bactiar291@users.noreply.github.com>
2024-11-04 00:55:06 +0700

    Update account.js

--- account.js
+++ account.js
@@ -1,4 +1,4 @@
 export const acc = [
-  "namahotkamu.tg|ed25519:isi_private_key_disini-tapi janagan-hapus-|ed25519:-sebelum private key|1", // 1 jam delay bebas atur waktu
+  "namahotkamu.tg|ed25519:isi_private_key_disini-tapi jangan-hapus-ed25519nya|1", // 1 jam delay bebas atur waktu
   // Tambahkan akun lainnya sesuai kebutuhan
 ];
